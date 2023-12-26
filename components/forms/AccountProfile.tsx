686fc93d4e65fa57909c5369ad1ca66b88e3b0e9
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2023-12-26 23:58:05 +0530

    Uploadthing

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -17,8 +17,10 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from '@/lib/uploadthing'
 
 interface Props {
     user: {
@@ -35,25 +37,60 @@ interface Props {
 
 const AccountProfile = ({user, btnTitle} : Props) => {
 
+    const [files, setFiles] = useState<File[]>([]);
+    const { startUpload } = useUploadThing("media");
+
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
-            profile_photo: "",
-            name: "",
-            username: "",
-            bio: "",
+            profile_photo: user?.image || "",
+            name: user?.name || "",
+            username: user?.username || "",
+            bio: user?.bio || "",
         }
     });
 
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
+
+        const fileReader = new FileReader();
+
+        if(e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+
+            setFiles(Array.from(e.target.files));
+            
+            if(!file.type.includes('image'))    return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || '';
+
+                //  allows us to update the field using react-forms method
+                fieldChange(imageDataUrl);
+            }
+
+            fileReader.readAsDataURL(file);
+        }
     }
 
-function onSubmit(values: z.infer<typeof UserValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    
+    const blob = values.profile_photo;
+
+    const hasImageChanged = isBase64Image(blob);
+    if(hasImageChanged)
+    {
+        const imgRes = await startUpload(files);
+        if(imgRes && imgRes[0].url)
+        {
+            values.profile_photo = imgRes[0].url;
+        }
+    }
+
+    //  TODO: Update user profile
+
     console.log(values)
-      }
+}
 
     return (
         <Form {...form}>
@@ -104,11 +141,11 @@ function onSubmit(values: z.infer<typeof UserValidation>) {
                 control={form.control}
                 name="name"
                 render={({ field }) => (
-                    <FormItem className="flex items-center gap-3 w-full">
+                    <FormItem className="flex flex-col gap-3 w-full">
                         <FormLabel className="text-base-semibold text-light-2">
                             Name
                         </FormLabel>
-                        <FormControl className="flex-1 text-base-semibold text-gray-200">
+                        <FormControl>
                             <Input
                                 type="text"
                                 className="account-form_input no-focus"
@@ -123,11 +160,11 @@ function onSubmit(values: z.infer<typeof UserValidation>) {
                 control={form.control}
                 name="username"
                 render={({ field }) => (
-                    <FormItem className="flex items-center gap-3 w-full">
+                    <FormItem className="flex flex-col gap-3 w-full">
                         <FormLabel className="text-base-semibold text-light-2">
                             Username
                         </FormLabel>
-                        <FormControl className="flex-1 text-base-semibold text-gray-200">
+                        <FormControl>
                             <Input
                                 type="text"
                                 className="account-form_input no-focus"
@@ -142,11 +179,11 @@ function onSubmit(values: z.infer<typeof UserValidation>) {
                 control={form.control}
                 name="bio"
                 render={({ field }) => (
-                    <FormItem className="flex items-center gap-3 w-full">
+                    <FormItem className="flex flex-col gap-3 w-full">
                         <FormLabel className="text-base-semibold text-light-2">
                             Bio
                         </FormLabel>
-                        <FormControl className="flex-1 text-base-semibold text-gray-200">
+                        <FormControl>
                             <Textarea
                                 rows={10}
                                 className="account-form_input no-focus"
