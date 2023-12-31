d7f344dc495b158e5a86f2493173af7109930ec4
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2023-12-31 16:00:49 +0530

    building it for community access

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -150,3 +150,32 @@ export async function fetchUsers({
     throw new Error(`Error fetching users : ${error.message}`)
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    
+    connectToDb();
+
+    //  find all threads created by the user
+    const userThreads = await Thread.find({author: userId});
+
+    //  Collect all the child thread ids (replies) from the 'children' field
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children)
+    }, [])
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }
+    }).populate({
+      path: 'author',
+      model: User,
+      select: 'name image _id'
+    })
+
+    return replies;
+
+  } catch (error: any) {
+      throw new Error(`Error fetching activity : ${error.message}`)
+  }
+}
